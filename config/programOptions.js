c281c7b9f6140c2845d93488affb57f83d143963
Rahul Prasad <rahulprasad4968@gmail.com>
2026-07-06 13:07:35 +0530

    Add register dropdowns for branch and year

---
+++ config/programOptions.js
@@ -0,0 +1,17 @@
+const yearOptions = ['1', '2', '3', '4'];
+
+const branchOptions = [
+  'B.Tech in Computer Science and Engineering',
+  'B.Tech in Computer Science and Engineering (Artificial Intelligence and Data Science)',
+  'B.Tech in Computer Science and Engineering (Artificial Intelligence and Machine Learning)',
+  'B.Tech in Computer Science and Engineering (Data Science and Analytics)',
+  'B. Tech in Computer Science & Engineering (Industry Integration Program with SAP)',
+  'B. Tech in Electronics and Computer Science (IBM Powered)',
+  'B. Tech in Electronics Biomedical Engineering',
+  'B. Tech in Electronics and Computer Science',
+  'B. Tech in Electrical Engineering (AI and Applications)',
+  'B. Tech in Robotics and AI',
+  'B. Tech in Civil Engineering',
+];
+
+module.exports = { yearOptions, branchOptions };
